7bf5f3002ecdaaac30e7811e517fcbca3a1b4ceb
xorms571 <xorms6865@naver.com>
2024-11-13 08:25:30 +0900

    ffinal3

--- routes/token.js
+++ routes/token.js
@@ -1,22 +1,21 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
 const { generateAccessToken } = require("../middlewares/generateToken");
 router.post("/", (req, res) => {
   const { refreshToken } = req.cookies;
 
-  if (!refreshToken) return res.sendStatus(401);
+  if (!refreshToken) {
+    return res.sendStatus(401);
+  } else {
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user) => {
+      if (err) return res.sendStatus(403);
 
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user) => {
-    if (err) {
-      console.error("JWT verification failed:", err); // 오류 메시지 출력
-      return res.sendStatus(403);
-    }
-
-    const accessToken = generateAccessToken({
-      userId: user.userId,
-      username: user.username,
+      const accessToken = generateAccessToken({
+        userId: user.userId,
+        username: user.username,
+      });
+      res.json({ accessToken });
     });
-    res.json({ accessToken });
-  });
+  }
 });
 module.exports = router;
